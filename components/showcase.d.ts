9d330e37ad2b1d536e1d208bc4abecbfc3e1d1af
Michael Klein <michael@firstiwaslike.com>
2020-05-13 17:10:28 +0200

    fix: failing linting for showcase types

--- components/showcase.d.ts
+++ components/showcase.d.ts
@@ -1,22 +1,22 @@
 import Component from "@glimmer/component";
 declare type SnippetDescription = {
-    name: string;
-    title?: string;
-    language: string;
+  name: string;
+  title?: string;
+  language: string;
 };
 export declare type Snippet = {
-    name: string;
-    title: string;
-    language: string;
+  name: string;
+  title: string;
+  language: string;
 };
-export default class Demo extends Component {
-    snippets: Snippet[];
-    activeSnippet: Snippet | null;
-    snippetComponent: string;
-    useSnippetComponent: string;
-    get _activeSnippet(): Snippet | undefined;
-    registerSnippet(snippetDescription: SnippetDescription): void;
-    activateSnippet(snippet: Snippet): void;
-    private generateSnippetFromDescription;
+export default class Showcase extends Component {
+  snippets: Snippet[];
+  activeSnippet: Snippet | null;
+  snippetComponent: string;
+  useSnippetComponent: string;
+  get _activeSnippet(): Snippet | undefined;
+  registerSnippet(snippetDescription: SnippetDescription): void;
+  activateSnippet(snippet: Snippet): void;
+  private generateSnippetFromDescription;
 }
 export {};
