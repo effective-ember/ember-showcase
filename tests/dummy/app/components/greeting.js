0b69a88be66dfdd5ea157788b60363dbdf5f6500
pangratz <cmueller.418@gmail.com>
2020-06-01 21:27:19 +0200

    DOC: outline basic idea behind addon at top of documentation page
    This adds a sample of what this addon can produce on top of the
    documentation page. By this the use case for this addon is demonstrated
    first, before the documentation dives into how the addon is used.

---
+++ tests/dummy/app/components/greeting.js
@@ -0,0 +1,17 @@
+import Component from "@glimmer/component";
+
+// BEGIN-SNIPPET greeting-component
+export default class Greeting extends Component {
+  get name() {
+    if (!this.args.name) {
+      return "<no @name provided>";
+    }
+
+    if (this.args.uppercase) {
+      return this.args.name.toUpperCase();
+    }
+
+    return this.args.name;
+  }
+}
+// END-SNIPPET
