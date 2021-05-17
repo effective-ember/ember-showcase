79ae6033d808611c8a44964522dde9d9139e144e
pangratz <cmueller.418@gmail.com>
2021-05-17 15:56:05 +0200

    chore: skip babel for qunit package when using embroider
    This fixes the issue outlined in https://github.com/embroider-build/embroider/issues/677

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -55,5 +55,11 @@ module.exports = function (defaults) {
   */
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };
