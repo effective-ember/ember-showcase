b568e06b5ac13edfcbc5e1e0b6a70792b03d8b5f
Michael Klein <michael@firstiwaslike.com>
2021-05-17 10:56:32 +0200

    chore: ugprade ember-cli-typescript

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,6 @@
 // Types for compiled templates
-declare module "ember-showcase/templates/*" {
-  import { TemplateFactory } from "htmlbars-inline-precompile";
+declare module 'ember-showcase/templates/*' {
+  import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
 }
